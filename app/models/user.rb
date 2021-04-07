6d7d4648a6ad81eaa1a67afbaf203c953a8ff8a8
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-07 14:19:56 +0900

    最終訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :birthday
     validates :first_name,:last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } 
     validates :first_furigana,:last_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } 
-    validates :encrypted_password,:password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/,message: "全て半角で英数字を混ぜて設定してください"}
+    validates :password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/,message: "全て半角で英数字を混ぜて設定してください"}
   end
 
   #has_many :items
