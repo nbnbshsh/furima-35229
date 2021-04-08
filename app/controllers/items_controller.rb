3c98c3bc3c92d949717538ff556678c0a94b884b
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-08 14:40:53 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,31 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new,:create]
-  
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    @items=Item.all.order(created_at: "DESC")
+    @items = Item.all.order(created_at: 'DESC')
   end
 
   def new
-    @item=Item.new
+    @item = Item.new
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def create
-    @item=Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
     end
   end
-  
+
   private
 
   def item_params
-    params.require(:item).permit(:item_name,:explain,:category_id,:condition_id,:delivery_fee_id,:prefecture_id,:delivery_date_id,:price,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explain, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_date_id,
+                                 :price, :image).merge(user_id: current_user.id)
   end
-
 end
--- app/models/delivery_date.rb
+++ app/models/delivery_date.rb
@@ -3,8 +3,8 @@ class DeliveryDate < ActiveHash::Base
     { id: 1, name: '--' },
     { id: 2, name: '１〜２日で発送' },
     { id: 3, name: '２〜3日で発送' },
-    { id: 4, name: '４〜7日で発送' },
+    { id: 4, name: '４〜7日で発送' }
   ]
   include ActiveHash::Associations
   has_many :items
-  end
+end
