a4f1bef0b58156fcd1e6708a595ee7de5bbae133
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-08 16:10:32 +0900

    訂正１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create,:edit,:update]
-
+  before_action :set_items,only: [:show,:edit,:update]
+  before_action :editting_items,only: [:edit,:update]
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -10,7 +11,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
@@ -23,14 +23,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item=Item.find(params[:id])
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
+   
   end
 
   def update
-    @item=Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,5 +39,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :explain, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_date_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_items
+    @item=Item.find(params[:id])
+  end
+
+  def editting_items
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
