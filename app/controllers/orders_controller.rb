2d19a81faa0d85731f1e9bfc53e3065ff7b2e88a
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-09 17:41:57 +0900

    addressへの保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = 'sk_test_a1e8f03a8a226752149bb151'  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = 'sk_test_a1e8f03a8a226752149bb151'
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
       card: order_params[:token], # カードトークン
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,15 +8,14 @@ class OrderAddress
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
-    validates :telephone_number, format: { with: /\A\d{11}\z/ }
+    validates :telephone_number, format: { with: /\A\d{,11}\z/ }
     validates :token
     validates :user_id
     validates :item_id
   end
 
   def save
-    Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
-                   building_name: building_name, telephone_number: telephone_number)
+    order=Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,building_name: building_name, telephone_number: telephone_number,order_id: order.id)
   end
 end
