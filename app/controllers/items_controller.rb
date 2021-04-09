365db6e0c83cc04122b3853b51462957e566bef8
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-09 14:57:48 +0900

    購入機能 訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_items, only: [:show, :edit, :update, :destroy]
   before_action :editting_items, only: [:edit, :update, :destroy]
-
+  
+  
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -54,4 +55,6 @@ class ItemsController < ApplicationController
   def editting_items
     redirect_to root_path unless current_user.id == @item.user_id
   end
+
+  
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  before_action :redireting_soldout,only: [:index,:create]
 
   def index
     @order_address = OrderAddress.new
@@ -27,6 +28,12 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def redireting_soldout
+    if @item.order != nil
+    return redirect_to root_path
+    end 
   end
 
   def pay_item
