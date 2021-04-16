8ed60b2fcc69e50fb11cc2795d420186d12e7642
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-16 15:03:19 +0900

    “空のコミット”

--- db/migrate/20210408094646_create_addresses.rb
+++ db/migrate/20210408094646_create_addresses.rb
@@ -7,7 +7,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string       :address,            null: false                    
       t.string       :building_name     
       t.string       :telephone_number,   null: false                  
-      t.references   :user,     null: false, foreign_key: true 
+      t.references   :user,               null: false, foreign_key: true 
       t.timestamps
     end
   end
