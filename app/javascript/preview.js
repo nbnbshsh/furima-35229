dac065b632c151b5a3fc27669ca07946f9d45480
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-04-17 11:39:11 +0900

    コメント複数投稿機能
    newとeditでJSの条件分ける
    editはtagを修正できないためparamsを分けている。
    そのため、item[images][]とitems_tag[images][]と違いが発生する

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,4 +1,4 @@
-if (document.URL.match(/new/)||document.URL.match(/edit/)) {
+if (document.URL.match(/new/)) {
 document.addEventListener('DOMContentLoaded',function(){
   const ImageList = document.getElementById('image-list');
    
@@ -36,3 +36,42 @@ document.addEventListener('DOMContentLoaded',function(){
   });
 });
 }
+
+if (document.URL.match(/edit/)) {
+  document.addEventListener('DOMContentLoaded',function(){
+    const ImageList = document.getElementById('image-list');
+     
+    const createImageHTML=(blob) => {
+    const imageElement = document.createElement('div');
+    imageElement.setAttribute('class', "image-element")
+    let imageElementNum = document.querySelectorAll('.image-element').length
+  
+        const blobImage = document.createElement('img');
+        blobImage.setAttribute('src',blob);
+        blobImage.setAttribute('class','new-image');
+  
+        const inputHTML =document.createElement('input')
+        inputHTML.setAttribute('id', `item-image_${imageElementNum}`)
+        inputHTML.setAttribute('name', 'item[images][]')
+        inputHTML.setAttribute('type', 'file')
+  
+        imageElement.appendChild(blobImage)
+        imageElement.appendChild(inputHTML)
+        ImageList.appendChild(imageElement)
+  
+        inputHTML.addEventListener('change', (e) => {
+          file = e.target.files[0];
+          blob = window.URL.createObjectURL(file);
+  
+          createImageHTML(blob)
+        })
+  }
+    document.getElementById('item-image').addEventListener('change',(e) => {
+  
+        let file= e.target.files[0];
+        let blob = window.URL.createObjectURL(file);
+  
+        createImageHTML(blob);
+    });
+  });
+  }
